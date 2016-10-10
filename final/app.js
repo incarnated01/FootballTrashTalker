e51726e6b0f625dc87440f5010b2a150ca7a56a2
ericflynn1 <ericgflynn@gmail.com>
2016-10-10 10:44:07 -0400

    Frontend initial

---
+++ final/app.js
@@ -0,0 +1,64 @@
+var stompClient = null;
+
+function setConnected(connected) {
+    $("#connect").prop("disabled", connected);
+    $("#disconnect").prop("disabled", !connected);
+    if (connected) {
+        $("#conversation").show();
+    }
+    else {
+        $("#conversation").hide();
+    }
+    $("#greetings").html("");
+}
+
+// function connect() {
+//     var socket = new SockJS('/ws');
+//     stompClient = Stomp.over(socket);
+//     stompClient.connect({}, function (frame) {
+//         setConnected(true);
+//         console.log('Connected: ' + frame);
+//         stompClient.subscribe('/topic/teamId1', function (message) {
+//             showGreeting(JSON.parse(message.body).text);
+//         });
+//     });
+// }
+
+function disconnect() {
+    if (stompClient != null) {
+        stompClient.disconnect();
+    }
+    setConnected(false);
+    console.log("Disconnected");
+}
+
+function sendMessage() {
+    stompClient.send("/app/teamId1", {}, JSON.stringify({'text': $("#message").val()}));
+}
+
+function showGreeting(message) {
+    $("#greetings").append("<tr><td>" + message + "</td></tr>");
+}
+
+$(function () {
+    $("form").on('submit', function (e) {
+        e.preventDefault();
+    });
+    $( "#connect" ).click(function() { connect(); });
+    $( "#disconnect" ).click(function() { disconnect(); });
+    $( "#send" ).click(function() { sendMessage(); });
+});
+
+window.addEventListener('load', function () {
+    function connect() {
+    var socket = new SockJS('/ws');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+        setConnected(true);
+        console.log('Connected: ' + frame);
+        stompClient.subscribe('/topic/teamId1', function (message) {
+            showGreeting(JSON.parse(message.body).text);
+        });
+    });
+}
+});
