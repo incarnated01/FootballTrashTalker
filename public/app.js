77544ab29d5fcd231eda666f13977519067b9667
ericflynn1 <ericgflynn@gmail.com>
2016-10-12 15:33:31 -0400

    checking functionality

--- public/app.js
+++ public/app.js
@@ -1,6 +1,23 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var stompClient = null;
 
+let app = angular.module('sportsApp', ['ui.router']);
+
+app.factory("messageService", function () {
+    let active = 'matchRoom';
+
+    let rooms = {
+         matchRoom: ['oogabooga'],
+         teamRoom: [],      
+    };
+
+    return {
+        getCurrent: function() {
+            return rooms[active];
+        },
+    };
+});
+
 function disconnect() {
     if (stompClient != null) {
         stompClient.disconnect();
