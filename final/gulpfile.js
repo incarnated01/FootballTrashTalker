0461f72a5509ae8c015b6e808222ee7512a84090
ericflynn1 <ericgflynn@gmail.com>
2016-10-14 13:01:20 -0400

    changing rooms works

--- final/gulpfile.js
+++ final/gulpfile.js
@@ -7,23 +7,25 @@ gulp.task('default', ['html', 'scss', 'js']);
 
 gulp.task('html', function () {
     return gulp.src('index.html')
-        .pipe(gulp.dest('public'));
+        //    gulp.src('login.html')
+        .pipe(gulp.dest('../public'));
 })
 
 gulp.task('scss', function () {
     return gulp.src('style.scss')
         .pipe(sass())
-        .pipe(gulp.dest('public'));
+        .pipe(gulp.dest('src/main/resources/public'));
 })
 
 gulp.task('js', function () {
     return gulp.src('app.js')
         .pipe(browserify())
-        .pipe(gulp.dest('public'));
+        .pipe(gulp.dest('src/main/resources/public'));
         })
 
 gulp.task('watch', function () {
             gulp.watch('index.html', ['html']);
+            gulp.watch('login.html', ['html']);
             gulp.watch('*.scss', ['scss']);
             gulp.watch('app.js', ['js']);
         });
