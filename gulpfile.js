f7e2841ca067a6271f9b0915c95c2c56c8ceabc2
ericflynn1 <ericgflynn@gmail.com>
2016-10-18 10:49:03 -0400

    It's mvp

--- gulpfile.js
+++ gulpfile.js
@@ -3,29 +3,34 @@ let gulp = require('gulp');
 let sass = require('gulp-sass');
 let browserify = require('gulp-browserify');
 
-gulp.task('default', ['html', 'scss', 'js']);
+gulp.task('default', ['html', 'scss', 'js', 'components']);
 
 gulp.task('html', function () {
-    return gulp.src('index.html')
-           gulp.src('login.html')
-        .pipe(gulp.dest('public'));
-})
+    return gulp.src(['index.html', 'login.html'])
+        .pipe(gulp.dest('../src/main/resources/templates'));
+});
+
+gulp.task('components', function () {
+    return gulp.src('components/*')
+        .pipe(gulp.dest('../src/main/resources/public/components'));
+});
 
 gulp.task('scss', function () {
     return gulp.src('style.scss')
         .pipe(sass())
-        .pipe(gulp.dest('public'));
-})
+        .pipe(gulp.dest('../src/main/resources/public'));
+});
 
 gulp.task('js', function () {
     return gulp.src('app.js')
         .pipe(browserify())
-        .pipe(gulp.dest('public'));
-        })
+        .pipe(gulp.dest('../src/main/resources/public'));
+});
 
 gulp.task('watch', function () {
-            gulp.watch('index.html', ['html']);
-            gulp.watch('login.html', ['html']);
-            gulp.watch('*.scss', ['scss']);
-            gulp.watch('app.js', ['js']);
-        });
+    gulp.watch('index.html', ['html']);
+    gulp.watch('login.html', ['html']);
+    gulp.watch('components/*', ['components']);
+    gulp.watch('*.scss', ['scss']);
+    gulp.watch('app.js', ['js']);
+});
